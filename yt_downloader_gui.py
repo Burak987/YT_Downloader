d518157252f2af7b33bb2ab6368c3f37abc77677
Burak987 <ales.rapek@seznam.cz>
2025-09-27 22:30:41 +0200

    new

--- yt_downloader_gui.py
+++ yt_downloader_gui.py
@@ -8,7 +8,7 @@ import urllib.request
 import webbrowser
 
 # ==== Verze a URL k aktualizaci ====
-APP_VERSION = "0.1.0"
+APP_VERSION = "0.1.1"
 VERSION_URL = "https://raw.githubusercontent.com/Burak987/YT_Downloader/main/version.txt"
 UPDATE_URL = "https://github.com/Burak987/YT_Downloader/raw/refs/heads/main/yt_downloader_gui.exe"
 
